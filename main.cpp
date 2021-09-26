7d18aaaf638ce39e0d3d692de8005ec2d9b9d04c
ceeedge <cedricharper@icloud.com>
2021-09-26 16:38:02 -0400

    added surface rendering normal equation

--- main.cpp
+++ main.cpp
@@ -4,24 +4,34 @@
 
 #include <iostream>
 
-bool hitSphere(const Point3& center, double radius, const Ray& r)
+double hitSphere(const Point3& center, double radius, const Ray& r)
 {
     Vec3 oc{r.origin() - center};
     auto a{dot(r.direction(), r.direction())};
     auto b{2.0 * dot(oc, r.direction())};
     auto c{dot(oc, oc) - radius*radius};
     auto discriminant{b*b - 4*a*c};
-    return discriminant > 0; 
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - sqrt(discriminant) ) / (2.0 * a);
+    }
 }
 
 Color ray_color(const Ray& r)
 {
-    if(hitSphere(Point3{0,0,-1}, 0.5, r))
+    auto t{hitSphere(Point3{0, 0, -1}, 0.5, r)};
+    if (t > 0.0)
     {
-        return Color{1, 0, 0};
+        Vec3 N = unit_vector(r.at(t) - Vec3{0,0,-1});
+        return 0.5 * Color{N.x()+1, N.y()+1, N.z()+1};
     }
+
     Vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
+    t = 0.5*(unit_direction.y() + 1.0);
     return (1.0 - t) * Color{1.0, 1.0, 1.0} + 
         t * Color{0.5, 0.7, 1.0}; 
 }
